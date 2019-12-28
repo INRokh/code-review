2660efa817e938a7353bd91313ea4327f23425e0
Nina <ilnarnika@gmail.com>
2019-12-29 01:14:39 +1100

    Added authentication reducer.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header'
 
-const Landing = () => <h2>Landing</h2>;
-const Dashboard = () => <h2>Dashboard </h2>;
-const GroupNew = () => <h2>Create group</h2>
+// const Landing = () => <h2>Landing</h2>;
+//{/* <Route exact path='/' component={Landing} />  */}
 
-
-const App = () => {
-    return(
-        <div className='container'>
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path='/' component={Landing} /> 
-                    <Route exact path='/groups' component={Dashboard} />
-                    <Route exact path='/groups/new' component={GroupNew} />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+class App extends React.Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+    
+    render() {
+        return(
+            <div className='container'>
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 };
 
-export default App;
+export default connect(null, actions)(App);
